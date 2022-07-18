a2536b989317defdeea4f049034410cd50f4406a
Tsyriulnikov <max_djvu@mail.ru>
2022-07-18 16:49:34 +0300

    18_07_22_1

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Stock from "./components/Stock.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h2 class="title">Stocks</h2>
+    <Stock/>
+  </div>
+</template>
+
+
---
+++ src/components/Stock.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="stock">
+  <div class="stock-item">
+
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
