f92e3ccc609a48191ec8426ae48cc33dff2e0c51
Tsyriulnikov <max_djvu@mail.ru>
2022-07-19 10:01:04 +0300

    19_07_22_1

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -3,11 +3,20 @@
 </script>
 
 <template>
-<div class="stock">
-  <div class="stock-item">
-
+  <div class="stock">
+    <div class="stock-item">
+      <div class="stock-item_info">
+        <div class="stock-item_cover">
+          <img src="" alt="logo company">
+        </div>
+        <h3 class="stock-item_title">
+          Apple Inc
+          <span>AAPL</span>
+        </h3>
+      </div>
+      <span class="stock-item_price"></span>
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped>
